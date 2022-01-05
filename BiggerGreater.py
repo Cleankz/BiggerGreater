17ebad76a059361c3b1ce6791356824b5c9ea48e
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-01-05 16:18:40 +0300

    Create BiggerGreater.py

---
+++ BiggerGreater.py
@@ -0,0 +1,23 @@
+import random
+def BiggerGreater(inp):
+    count =  (len(inp) * (len(inp) - 1)) * len(inp)
+    varity = []
+    varity.append(inp)
+    string = list(inp)
+    flag = False
+    for i in range(count):
+        s1 = random.randint(0,(len(inp) - 1))
+        s2 = random.randint(0,(len(inp) - 1))
+        string[s1],string[s2] = string[s2],string[s1]
+        if "".join(string) not in varity:
+            varity.append("".join(string))
+    varity.sort()
+    for i in range(len(varity)):
+        if varity[i] > inp:
+            flag = True
+            result = varity[i]
+            break
+    if flag:
+        return result
+    else:
+        return ""
